6818ae0a461337966a3db97139fafecb96b7f885
Djenk Ivanov <djenk.ivanov@outlook.com>
2025-10-31 02:05:00 +0100

    create and deploy endpoint, scoring script

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, normalize
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from azure.identity import DefaultAzureCredential
@@ -16,13 +17,69 @@ from pathlib import Path
 
 
 def main():
+    def get_recommendations_for_user(user_id_enc, top_k=10):
+        liked = [i for i in liked_by_users[user_id_enc]]
+    
+        scores = np.zeros(len(indices), dtype=np.float32)
+        neighbors = indices[liked].ravel()
+        weights = sims[liked].ravel()
+        np.add.at(scores, neighbors, weights)
+        
+        scores[list(seen_by_user[user_id_enc])] = -np.inf
+        
+        top = np.argpartition(scores, -top_k)[-top_k:]
+        recommended_indices = top[np.argsort(-scores[top])]
+        return movies.iloc[recommended_indices][['movieId', 'title']].to_dict(orient='records')
+    
+    def evaluate(k):
+        users_evaluated = 0
+        users_with_hits = 0
+        sum_precision = 0.0
+        sum_recall = 0.0
+        total_hits = 0
+        total_liked = 0
+        
+        users = (u for u in liked_by_users.keys() if u in sample_users)
+        
+        for u in users:
+            liked_movies = liked_by_users[u]
+            print(f'Users evaluated: {users_evaluated}/{len(sample_users)}')
+            recs = get_recommendations_for_user(u, top_k=k)
+            if not recs.any():
+                continue
+            
+            users_evaluated += 1
+            num_hits = len(set(recs) & liked_movies)
+            
+            if num_hits > 0:
+                users_with_hits += 1
+                
+            sum_precision += num_hits / k
+            sum_recall += num_hits / len(liked_movies)
+            
+            total_hits += num_hits
+            total_liked += len(liked_movies)
+        
+        precision = sum_precision / users_evaluated
+        recall = sum_recall / users_evaluated
+        hitrate = users_with_hits / users_evaluated
+        
+        
+        return {
+            'precision': precision,
+            'recall': recall,
+            'hitrate': hitrate,
+            'total_hits': total_hits,
+            'total_liked': total_liked
+        }
+        
+        
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, help="Model name for MLflow registration")
     parser.add_argument("--data", type=str, help="Data asset input")
     args = parser.parse_args()
     
     mlflow.start_run()
-    mlflow.sklearn.autolog()
     
     data_asset = args.data
 
@@ -40,13 +97,17 @@ def main():
 
     df = merged.copy()
 
+    max_users = 100_000
+    sampled_users = np.random.choice(df['userId'].unique(), size=max_users, replace=False)
+    df = df[df['userId'].isin(sampled_users)].copy()
+        
     df['liked'] = (df['rating'] >= 3).astype('int8')
 
     row_hash = pd.util.hash_pandas_object(df[['userId', 'movieId', 'liked']], index=False).astype('uint64')
     rand = (row_hash % (2**32)) / (2**32)
 
     df['split'] = np.where(rand < 0.5, 'train', 'test')
-    
+
     train = df[df['split'] == 'train'].copy()
     test = df[df['split'] == 'test'].copy()
 
@@ -69,39 +130,58 @@ def main():
     total_movies = train['movieId_enc'].max() + 1
 
     X_train = csr_matrix(   
-        (np.ones(train_liked.shape[0]), (train_liked['userId_enc'].to_numpy(), train_liked['movieId_enc'].to_numpy())),
-        shape=(total_users, total_movies)
+        (np.ones(train_liked.shape[0], dtype=np.float32), (train_liked['userId_enc'].to_numpy(), train_liked['movieId_enc'].to_numpy())),
+        shape=(total_users, total_movies),
+        dtype=np.float32
     )
 
-    V_train = cosine_similarity(X_train.T, X_train.T, dense_output=False)
+    K = 100
+    X_movies = X_train.T.tocsr().astype(np.float32)
+    X_movies = normalize(X_movies, axis=1, copy=False)
+
+    nn = NearestNeighbors(n_neighbors=K+1, metric='cosine', algorithm='brute', n_jobs=-1)
+    nn.fit(X_movies)
+    
+    dists, indices = nn.kneighbors(X_movies, return_distance=True, n_neighbors=K+1)
+    sims = 1.0 - dists
+    
+    indices = indices[:, 1:]
+    sims = sims[:, 1:].astype(np.float32)
     
     seen_by_user = [set() for _ in range(total_users)]
     for u, m in zip(train['userId_enc'].to_numpy(), train['movieId_enc'].to_numpy()):
         seen_by_user[u].add(m)
             
-    liked_by_user = {}
+    liked_by_users = {}
 
     for u, m, l in zip(test['userId_enc'].to_numpy(), test['movieId_enc'].to_numpy(), test['liked'].to_numpy()):
         if l == 1:
-            liked_by_user.setdefault(u, set()).add(m)
+            liked_by_users.setdefault(u, set()).add(m)
             
-    all_eval_users = np.array(list(liked_by_user.keys()), dtype=np.int64)
+    all_eval_users = np.array(list(liked_by_users.keys()), dtype=np.int64)
     h = pd.util.hash_array(all_eval_users)
     sample_mask = (h % 1000) == 0
     sample_users = set(all_eval_users[sample_mask])
     
-    eval_results = evaluate(k=10, liked_by_user=liked_by_user, sample_users=sample_users)
+    recs = get_recommendations_for_user(user_id_enc=100, top_k=10)
+    print(recs)
     
-    mlflow.log_metrics(eval_results)
+    # eval_results = evaluate(k=10)
+    
+    # mlflow.log_metrics(eval_results)
     
     artifact_dir = Path("artifacts")
     artifact_dir.mkdir(exist_ok=True)
     
     joblib.dump(user_encoder, artifact_dir / "user_encoder.pkl")
     joblib.dump(movie_encoder, artifact_dir / "movie_encoder.pkl")
-    sp.save_npz(artifact_dir / "similarities.npz", V_train)
+    joblib.dump(seen_by_user, artifact_dir / "seen_by_user.pkl")
+    joblib.dump(liked_by_users, artifact_dir / "liked_by_users.pkl")
+    np.save(artifact_dir / "movie_knn_indices.npy", indcices.astype(np.int32))
+    np.save(artifact_dir / "movie_knn_similarities.npy", sims.astype(np.float32))
+    movies.to_parquet(artifact_dir / "movies.parquet", index=False)
     
-    mlflow.log_artifacts(artifact_dir, artifact_path="artifacts")
+    mlflow.log_artifacts(str(artifact_dir), artifact_path="artifacts")
     
     model_uri = f"runs:/{mlflow.active_run().info.run_id}/artifacts"
     
@@ -110,56 +190,6 @@ def main():
     mlflow.end_run()
     
     
-def get_recommendations_for_user(user_id_enc, top_k, seen_by_user, X_train, V_train):
-    liked = [i for i in seen_by_user[user_id_enc] if X_train[user_id_enc, i] > 0]
-    scores = V_train[:, liked].sum(axis=1).A.ravel()
-    if seen_by_user[user_id_enc]:
-        scores[list(seen_by_user[user_id_enc])] = -np.inf
-    top_indices = np.argpartition(scores, -top_k)[-top_k:]
-    return top_indices[np.argsort(-scores[top_indices])]
-
-    
-def evaluate(k, liked_by_user, sample_users):
-    users_evaluated = 0
-    users_with_hits = 0
-    sum_precision = 0.0
-    sum_recall = 0.0
-    total_hits = 0
-    total_liked = 0
-    
-    users = (u for u in liked_by_user.keys() if u in sample_users)
-    
-    for u in users:
-        liked_movies = liked_by_user[u]
-        print(f'Users evaluated: {users_evaluated}/{len(sample_users)}')
-        recs = get_recommendations_for_user(u, top_k=k)
-        if not recs.any():
-            continue
-        
-        users_evaluated += 1
-        num_hits = len(set(recs) & liked_movies)
-        
-        if num_hits > 0:
-            users_with_hits += 1
-            
-        sum_precision += num_hits / k
-        sum_recall += num_hits / len(liked_movies)
-        
-        total_hits += num_hits
-        total_liked += len(liked_movies)
-    
-    precision = sum_precision / users_evaluated
-    recall = sum_recall / users_evaluated
-    hitrate = users_with_hits / users_evaluated
-    
-    
-    return {
-        'precision': precision,
-        'recall': recall,
-        'hitrate': hitrate,
-        'total_hits': total_hits,
-        'total_liked': total_liked
-    }
     
 if __name__ == "__main__":
     main()
---
+++ src/score.py
@@ -0,0 +1,45 @@
+import joblib
+import numpy as np
+import pandas as pd
+import json
+import os
+
+def init():
+    global user_enc, movie_enc, movie_indices, movie_similarities, movies, seen_by_user, liked_by_users
+    
+    model_dir = os.getenv('AZUREML_MODEL_DIR')
+
+    user_enc = joblib.load(f'{model_dir}/artifacts/user_encoder.pkl')
+    movie_enc = joblib.load(f'{model_dir}/artifacts/movie_encoder.pkl')
+    
+    movie_indices = np.load(f'{model_dir}/artifacts/movie_knn_indices.npy')
+    movie_similarities = np.load(f'{model_dir}/artifacts/movie_knn_similarities.npy')
+    movies = pd.read_parquet(f'{model_dir}/artifacts/movies.parquet')
+    seen_by_user = joblib.load(f'{model_dir}/artifacts/seen_by_user.pkl')
+    liked_by_users = joblib.load(f'{model_dir}/artifacts/liked_by_users.pkl')    
+    
+
+def recommend_movie_to_user(user_id, top_k=10):
+    liked = [i for i in liked_by_users[user_id]]
+    
+    scores = np.zeros(len(movie_indices), dtype=np.float32)
+    neighbors = movie_indices[liked].ravel()
+    weights = movie_similarities[liked].ravel()
+    np.add.at(scores, neighbors, weights)
+    
+    scores[list(seen_by_user[user_id])] = -np.inf
+    
+    top = np.argpartition(scores, -top_k)[-top_k:]
+    recommended_indices = top[np.argsort(-scores[top])]
+    return movies.iloc[recommended_indices][['movieId', 'title']].to_dict(orient='records')
+
+
+def run(raw_data):
+    try:
+        data = json.loads(raw_data)
+        user_id = data['userId']
+        top_k = data.get('top_k', 10)
+        recommendations = recommend_movie_to_user(user_id, top_k=top_k)
+        return json.dumps({'recommendations': recommendations})
+    except Exception as e:
+        return json.dumps({"error": str(e)})
